f2237c575439dc86461e84085cf8b7883f4679ad
Valery Kalonga <valery@monaluxe.com>
2025-09-12 01:03:26 +0100

    docs(changelog): mise à jour [Unreleased] + ajustement main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'shared/navigation/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'features/profil/providers/profil_provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  
+  // Initialiser le formatage des dates pour le package intl
+  await initializeDateFormatting('fr', null);
 
   final urlFromDefine = const String.fromEnvironment('SUPABASE_URL');
   final keyFromDefine = const String.fromEnvironment('SUPABASE_ANON_KEY');
@@ -38,6 +43,9 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(appRouterProvider);
+    
+    // ⚠️ CORRECTIF : S'assurer que profilAuthSyncProvider est lu au boot
+    ref.watch(profilAuthSyncProvider);
     
     return MaterialApp.router(
       title: 'ML_PP MVP',
