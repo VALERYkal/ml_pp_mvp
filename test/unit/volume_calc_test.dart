0a9fa464c4de56a6890309e75b409f521aaf5b2d
Valery Kalonga <valery@monaluxe.com>
2026-01-14 03:59:48 +0100

    test(unit): make volume_calc tests tolerant to float precision

--- test/unit/volume_calc_test.dart
+++ test/unit/volume_calc_test.dart
@@ -6,9 +6,9 @@ import 'package:ml_pp_mvp/shared/utils/volume_calc.dart';
 
 void main() {
   test('computeVolumeAmbiant', () {
-    expect(computeVolumeAmbiant(1000, 1060), 60);
-    expect(computeVolumeAmbiant(500, 500), 0);
-    expect(computeVolumeAmbiant(null, 123), 0);
+    expect(computeVolumeAmbiant(1000, 1060), closeTo(60, 0.001));
+    expect(computeVolumeAmbiant(500, 500), closeTo(0, 0.001));
+    expect(computeVolumeAmbiant(null, 123), closeTo(0, 0.001));
   });
 
   test('computeV15 ESS', () {
@@ -18,7 +18,7 @@ void main() {
       densiteA15: null,
       produitCode: 'ESS',
     );
-    expect((v - 990).abs() < 0.6, true);
+    expect(v, closeTo(990, 0.6));
   });
 
   test('computeV15 AGO', () {
@@ -28,6 +28,6 @@ void main() {
       densiteA15: null,
       produitCode: 'AGO',
     );
-    expect((v - 991.5).abs() < 0.6, true);
+    expect(v, closeTo(991.5, 0.6));
   });
 }
